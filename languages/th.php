324f1db362e90490feb7771680963832663177c8
Marcus Povey <marcus@36083f99-b078-4883-b0ff-0f9b5a30f544>
2008-10-07 10:09:07 +0000

    Thai language pack

---
+++ languages/th.php
@@ -0,0 +1,26 @@
+<?php
+	/**
+	 * Elgg log browser plugin language pack
+	 * 
+	 * @package ElggPingBack
+	 * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU Public License version 2
+	 * @author Curverider Ltd
+	 * @copyright Curverider Ltd 2008
+	 * @link http://elgg.com/
+	 */
+
+	$thai = array(
+	
+		/**
+		 * Error messages
+		 */
+	
+			'pingback:error:missingparams' => 'ไม่มีพรารามิเตอร์',
+			'pingback:error:targetnotme' => ' URL เป้าหมายไม่สามารถยืนยันกลับมาได้',
+			'pingback:error:targetnotfoundinsource' => 'หาเป้าหมายไม่เจอ',
+	
+			'pingback:success' => 'รับปิงแบ็ค'
+	);
+					
+	add_translation("th",$thai);
+?>
